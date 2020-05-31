b2944a8d2ed9c9e90941445e67591cad2557b650
Shubham Agarwal <mwshubham@gmail.com>
2020-05-31 16:07:20 +0530

    added dagger scopes

---
+++ app/src/main/java/android/thegoodparts/di/scopes/ActivityScope.kt
@@ -0,0 +1,11 @@
+package android.thegoodparts.di.scopes
+
+import javax.inject.Scope
+
+/**
+ * Scope for a feature module.
+ */
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class ActivityScope
