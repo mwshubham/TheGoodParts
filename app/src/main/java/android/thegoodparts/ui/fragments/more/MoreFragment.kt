c7373e8dcc3734e0f08a9c0c3171c4f7e70eb111
Shubham Agarwal <mwshubham@gmail.com>
2020-05-31 18:45:59 +0530

    Added click listener on MoreFragment.kt

--- app/src/main/java/android/thegoodparts/ui/fragments/more/MoreFragment.kt
+++ app/src/main/java/android/thegoodparts/ui/fragments/more/MoreFragment.kt
@@ -2,19 +2,59 @@ package android.thegoodparts.ui.fragments.more
 
 import android.os.Bundle
 import android.thegoodparts.R
+import android.thegoodparts.databinding.FragmentMoreBinding
 import android.thegoodparts.ui.fragments.BaseFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import javax.inject.Inject
 
 class MoreFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentMoreBinding
+
+    @Inject
+    lateinit var viewModel: MoreFragmentVM
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_more, container, false
+        )
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        initClickListeners()
+        initObservers()
+    }
+
+    private fun initClickListeners() {
+        val showWipDialogFragmentClickListener = View.OnClickListener {
+            showWipDialogFragment()
+        }
+        binding.tvBookmark.setOnClickListener(showWipDialogFragmentClickListener)
+        binding.tvFilterContent.setOnClickListener(showWipDialogFragmentClickListener)
+        binding.tvWhatNew.setOnClickListener(showWipDialogFragmentClickListener)
+        binding.tvWhatUpcoming.setOnClickListener { showWipDialogFragment() }
+        binding.tvSettings.setOnClickListener { showWipDialogFragment() }
+        binding.tvSendFeedback.setOnClickListener { showWipDialogFragment() }
+        binding.tvHelpSupport.setOnClickListener { showWipDialogFragment() }
+    }
+
+    private fun initObservers() {
+    }
+
+    private fun showWipDialogFragment() {
+        findNavController().navigate(R.id.dialog_fragment_wip)
+    }
 }
--- app/src/main/java/android/thegoodparts/ui/fragments/more/MoreFragmentVM.kt
+++ app/src/main/java/android/thegoodparts/ui/fragments/more/MoreFragmentVM.kt
@@ -1,8 +1,18 @@
 package android.thegoodparts.ui.fragments.more
 
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 class MoreFragmentVM
-@Inject constructor(
-) : ViewModel()
+@Inject constructor() : ViewModel() {
+
+    init {
+        Timber.i("init{}")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("onCleared()")
+    }
+}
